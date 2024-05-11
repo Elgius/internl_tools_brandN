bce21d461e0d14995b9ce4936974c5f781b3d7f1
Elgius <114131653+Elgius@users.noreply.github.com>
2024-05-11 16:25:21 +0500

    we so back

---
+++ src/app/attendance/[name]/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import "./hamster.modules.css";
+import { supabaseClient } from "@/db/supabaseClient";
+
+const currentDate = new Date().toISOString().split("T")[0];
+
+function Data({ params }: { params: { name: string } }) {
+  // loader run to allow the site to check with the main server if todays date has been registered
+  const [isLoading, setIsLoading] = useState(false);
+
+  // code to check if todays date has been added to the db or not. if no, then the data is updated
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: record, error } = await supabaseClient
+          .from("Sample")
+          .select("*")
+          .eq("date", currentDate);
+
+        if (error) {
+          console.error("Error fetching record: ", error);
+          return;
+        }
+
+        if (!record || record.length === 0) {
+          const { error: insertError } = await supabaseClient
+            .from("Sample")
+            .insert([{ date: currentDate }])
+            .single();
+
+          if (insertError) {
+            console.error("Error inserting the record", insertError);
+            return;
+          }
+
+          console.log('Record inserted and "data" column updated.');
+        } else {
+          console.log("Record already exists.");
+        }
+      } catch (error) {
+        console.error("An unexpected error occurred:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading === true) {
+    return (
+      <>
+        <div className="flex items-center justify-center min-h-screen overflow-hidden">
+          <div>
+            <div
+              aria-label="Orange and tan hamster running in a metal wheel"
+              role="img"
+              className="wheel-and-hamster"
+            >
+              <div className="wheel"></div>
+              <div className="hamster">
+                <div className="hamster__body">
+                  <div className="hamster__head">
+                    <div className="hamster__ear"></div>
+                    <div className="hamster__eye"></div>
+                    <div className="hamster__nose"></div>
+                  </div>
+                  <div className="hamster__limb hamster__limb--fr"></div>
+                  <div className="hamster__limb hamster__limb--fl"></div>
+                  <div className="hamster__limb hamster__limb--br"></div>
+                  <div className="hamster__limb hamster__limb--bl"></div>
+                  <div className="hamster__tail"></div>
+                </div>
+              </div>
+              <div className="spoke"></div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="text-center">
+      <h1>Hello this {params.name} </h1>
+    </div>
+  );
+}
+
+export default Data;
--- src/app/attendance/page.tsx
+++ src/app/attendance/page.tsx
@@ -1,109 +1,27 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
-import { supabaseClient } from "@/db/supabaseClient";
+import Link from "next/link";
 
-function Page() {
-  const [userName, setUserName] = useState("");
-  const [lastUpdateTime, setLastUpdateTime] = useState(null);
-
-  useEffect(() => {
-    const fetchLastUpdateTime = async () => {
-      const { data, error } = await supabaseClient
-        .from("trial")
-        .select("last_update_time")
-        .eq("name", userName)
-        .maybeSingle();
-
-      if (error) {
-        console.error("Error fetching last update time: ", error.message);
-        return;
-      }
-      setLastUpdateTime(data?.last_update_time || null);
-    };
-    fetchLastUpdateTime();
-  }, [userName]);
-
-  const date = new Date();
-  let timestamp = date.toISOString();
-  console.log(timestamp);
-
-  const updateTimeStamp = async (buttonName) => {
-    const currentTime = new Date();
-    const midnight = new Date(
-      currentTime.getFullYear(),
-      currentTime.getMonth(),
-      currentTime.getDate()
-    );
-
-    if (
-      !lastUpdateTime ||
-      currentTime.getTime() >
-        new Date(lastUpdateTime).getTime() + 24 * 60 * 60 * 1000 ||
-      currentTime.getHours() >= 23
-    ) {
-      try {
-        const { data, error } = await supabaseClient.from("trial").upsert(
-          {
-            name: userName,
-            [buttonName]: new Date().toISOString(),
-            last_update_time: new Date().toISOString(),
-          },
-          { returning: "minimal" }
-        );
-
-        if (error) throw error;
-
-        console.log("Update successful:", data);
-      } catch (error) {
-        console.error("Error updating timestamp:", error.message);
-      }
-    } else {
-      console.log("Cannot update until tomorrow or after 11:59 PM today.");
-    }
-  };
-
+const page = () => {
   return (
-    <>
-      <div className="flex items-center justify-center overflow-hidden min-h-screen flex-col">
-        <main className="container text-center space-y-5">
-          <div>
-            <input
-              type="text"
-              placeholder="Enter your name"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Current time: {timestamp}</p>
-          </div>
-          <div>
-            <Button onClick={() => updateTimeStamp("sign_in")}>Sign in</Button>
-          </div>
-          <div>
-            <Button onClick={() => updateTimeStamp("lunch_In")}>
-              Lunch in
-            </Button>
-          </div>
-          <div>
-            <Button onClick={() => updateTimeStamp("lunch_out")}>
-              Lunch out
-            </Button>
-          </div>
-          <div>
-            <Button onClick={() => updateTimeStamp("sign_out")}>
-              Sign out{" "}
-            </Button>
-          </div>
-          <div>
-            <Button>Receipt </Button>
-          </div>
-        </main>
+    <div className="flex items-center justify-center min-h-screen overflow-hidden">
+      <div className="container text-center flex-col space-y-7">
+        <div>Choose who you are</div>
+        <div>
+          <Link href="/attendance/Elgius">
+            {" "}
+            <Button>Elgius</Button>{" "}
+          </Link>
+        </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
-export default Page;
+export default page;
+
+// TODO:
+// create a function to check if todays date is registered or not []
+// create a function to add a sign in data to that record []
+// create a function to add sign out data to that record []
+// create a pull record to check everyones record for that month []
